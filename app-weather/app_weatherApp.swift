3fbafc79dbddff218b6a2a0a1e8bad154f5a7702
mansmahamat <mansour.mahamat.salle@gmail.com>
2024-03-29 15:02:28 +0100

    Initial Commit

---
+++ app-weather/app_weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  app_weatherApp.swift
+//  app-weather
+//
+//  Created by Mansour Mahamat-salle on 29/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct app_weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
